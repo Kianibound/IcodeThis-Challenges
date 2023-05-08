ed89f1a7dfadbd3a1d94058ef598b68b70525324
kianibound <kianibound@gmail.com>
2023-05-09 00:14:48 +0330

    added message box

--- message-box/src/components/MainBox.tsx
+++ message-box/src/components/MainBox.tsx
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import MessageBox from "./MessageBox";
 
 function MainBox() {
   return (
-    <div className="bg-slate-200 w-1/3 h-96 p-4 rounded-lg flex justify-center">
+    <div className="bg-slate-200 w-1/3 h-96 p-4 rounded-lg flex flex-col items-center">
       <Header />
+      <MessageBox />
     </div>
   );
 }
---
+++ message-box/src/components/MessageBox.tsx
@@ -0,0 +1,15 @@
+const MessageBox = () => {
+  return (
+    <div>
+      <ul>
+        <li>Testing ul list</li>
+        <li>Testing ul list</li>
+        <li>Testing ul list</li>
+        <li>Testing ul list</li>
+        <li>Testing ul list</li>
+      </ul>
+    </div>
+  );
+};
+
+export default MessageBox;

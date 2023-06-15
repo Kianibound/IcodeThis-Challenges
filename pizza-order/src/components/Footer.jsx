1963dbb696b57cc53a145352343f92a794699de6
kianibound <kianibound@gmail.com>
2023-06-15 16:28:36 +0330

    finished but do not render pics

--- pizza-order/src/components/Footer.jsx
+++ pizza-order/src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const hour = new Date().getHours();
+  open = hour > 8 && hour < 18 ? true : false;
+  return (
+    <div>
+      <h1>Footer</h1>
+      <p>It is currently about {hour} o'clock!</p>
+      <p>We are currently {open ? "open" : "closed"}.</p>
+    </div>
+  );
 };
 
 export default Footer;

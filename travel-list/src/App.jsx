73da0251b47f343668e75d474c021085627eab9f
kianibound <kianibound@gmail.com>
2023-06-17 16:27:47 +0330

    initialized travel list app

---
+++ travel-list/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  );
+}
+
+export default App;
+
+const Logo = () => {
+  return <div>🌴 Far Away 💼</div>;
+};
+
+const Form = () => {
+  return <div className="add-form">
+    <h3>What do you need for 🧳 packing?</h3>
+  </div>;
+};

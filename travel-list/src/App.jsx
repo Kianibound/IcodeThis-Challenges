bb6b1c928f89615bdea8348187aaf649d558020b
kianibound <kianibound@gmail.com>
2023-06-18 13:54:09 +0330

    added some styles and data

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -1,19 +1,61 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Pants", quantity: 2, packed: true },
+];
+
 function App() {
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Footer />
+    </div>
   );
 }
 
 export default App;
 
 const Logo = () => {
-  return <div>🌴 Far Away 💼</div>;
+  return <h1>🌴 Far Away 💼</h1>;
 };
 
 const Form = () => {
-  return <div className="add-form">
-    <h3>What do you need for 🧳 packing?</h3>
-  </div>;
+  return (
+    <div className="add-form">
+      <h3>What do you need for 🧳 packing?</h3>
+    </div>
+  );
+};
+
+const PackingList = () => {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Item = ({ item }) => {
+  return (
+    <li style={item.packed ? { textDecoration: "line-through" } : {}}>
+      <span>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="stats">
+      <p>💼 You have X Items In your list and X Items Pakced (%X)</p>
+    </footer>
+  );
 };
